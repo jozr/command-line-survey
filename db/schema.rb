336afbc7c1551469855edd4a48453a929358f1ea
jozwright <jozwright@gmail.com>
2014-08-21 14:33:47 -0700

    adds answers table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821204522) do
+ActiveRecord::Schema.define(version: 20140821212245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.string   "answer",      limit: 1
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.integer  "survey_id"
