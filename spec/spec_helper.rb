c3e495ab5e3177fe3dd636d67e09dd9adf938741
jozwright <jozwright@gmail.com>
2014-08-24 10:16:01 -0700

    user specs passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'active_record'
 require 'rspec'
+require './lib/user'
+require './lib/survey'
+require './lib/question'
+require './lib/answer'
+require 'shoulda-matchers'
 
 database_configuration = YAML::load(File.open('./db/config.yml'))
 test_configuration = database_configuration['test']
@@ -7,6 +12,9 @@ ActiveRecord::Base.establish_connection(test_configuration)
 
 RSpec.configure do |config|
 	config.after(:each) do
-		Survey.all.each { |survey| survey.destroy }
+		Survey.all.each { |i| i.destroy }
+		User.all.each { |i| i.destroy }
+		Question.all.each { |i| i.destroy }
+		Answer.all.each { |i| i.destroy }
 	end
 end
