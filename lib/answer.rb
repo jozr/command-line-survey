5a5706d9287a4f7041ed5d897b82f941c3cbf4c3
jozwright <jozwright@gmail.com>
2014-08-24 10:25:33 -0700

    all specs passing

--- lib/answer.rb
+++ lib/answer.rb
@@ -1,4 +1,8 @@
 class Answer < ActiveRecord::Base
+	validates :user_id, :presence => true
+	validates :question_id, :presence => true
+	validates :answer, :presence => true
+
 	belongs_to :user
 	belongs_to :question
 end
---
+++ spec/answer_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Answer do
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :question_id }
+  it { should validate_presence_of :answer }
+
+  it { should belong_to :question }
+  it { should belong_to :user }
+end
