443fde141bd1443a31942320b539cca78bf516fa
jozwright <jozwright@gmail.com>
2014-08-28 11:10:06 -0700

    validate for length on answer; a OR b

--- lib/answer.rb
+++ lib/answer.rb
@@ -1,8 +1,14 @@
 class Answer < ActiveRecord::Base
 	validates :user_id, :presence => true
 	validates :question_id, :presence => true
-	validates :answer, :presence => true
+	validates :answer, :presence => true, :length => { maximum: 1 }
 
 	belongs_to :user
 	belongs_to :question
+
+private
+
+	def downcase_answer
+		answer.downcase!
+	end
 end
--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -4,6 +4,7 @@ describe Answer do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :answer }
+  it { should ensure_length_of(:answer).is_at_most(1) }
 
   it { should belong_to :question }
   it { should belong_to :user }
